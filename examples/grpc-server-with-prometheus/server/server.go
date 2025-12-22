0dbf89dee0bf3b742e52051bdf70c2d56d7c9ce5
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-12-22 13:58:31 +0100

    Revert "PMM-14577 Remove examples completely."
    This reverts commit e4f0538d2c7347023b9a9177420ac6cb5a92fc38.

--- examples/grpc-server-with-prometheus/server/server.go
+++ examples/grpc-server-with-prometheus/server/server.go
@@ -9,7 +9,7 @@ import (
 
 	"google.golang.org/grpc"
 
-	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	pb "github.com/grpc-ecosystem/go-grpc-prometheus/examples/grpc-server-with-prometheus/protobuf"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -72,8 +72,7 @@ func main() {
 	// Register your service.
 	pb.RegisterDemoServiceServer(grpcServer, demoServer)
 
-	// Initialize all metrics.
-	grpcMetrics.InitializeMetrics(grpcServer)
+	reg.MustRegister()
 
 	// Start your http server for prometheus.
 	go func() {
